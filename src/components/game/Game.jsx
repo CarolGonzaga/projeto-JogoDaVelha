1fa7fc8b6a2b96ee0a7b7490411d9e74c53737a6
CarolGonzaga <dev.carolgonzaga@gmail.com>
2024-04-18 10:24:46 -0300

    criado condicional do vencedor no css

--- src/components/game/Game.jsx
+++ src/components/game/Game.jsx
@@ -26,6 +26,7 @@ function Game() {
     const [gameState, setGameState] = useState(Array(9).fill(0))
     const [currentPlayer, setCurrentPlayer] = useState(-1)
     const [winner, setWinner] = useState(0)
+    const [winnerLine, setWinnerLine] = useState([])
 
     const handleClick = (pos) => {
         if (gameState[pos] === 0 && winner === 0) {
@@ -39,14 +40,21 @@ function Game() {
         winnerTable.forEach((line) => {
             const values = line.map((pos) => gameState[pos])
             const sum = values.reduce((sum, value) => sum + value)
-            if (sum === 3 || sum === -3) setWinner(sum / 3)
+            if (sum === 3 || sum === -3) {
+                setWinner(sum / 3)
+                setWinnerLine(line)
+            }
         })
     }
 
     const handleReset = () => {
         setGameState(Array(9).fill(0))
         setWinner(0)
+        setWinnerLine([])
     }
+
+    const verifyWinnerLine = (pos) => 
+        winnerLine.find((value) => value === pos) !== undefined
 
     useEffect(() => {
         setCurrentPlayer(currentPlayer * -1)
@@ -62,12 +70,13 @@ function Game() {
                             key={`game-option-pos-${pos}`}
                             status={value}
                             onClick={() => handleClick(pos)}
+                            isWinner={verifyWinnerLine(pos)}
                         />
                     )
                 }
             </div>
-            <GameInfo 
-                currentPlayer={currentPlayer} 
+            <GameInfo
+                currentPlayer={currentPlayer}
                 winner={winner}
                 onReset={handleReset}
             />
